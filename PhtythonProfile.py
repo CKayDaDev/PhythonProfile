1941cbcb6320f7585e0e00455e645b254b4dbb90
CKayDaDev <chikeechendu@gmail.com>
2025-09-06 18:50:12 +0100

    Add files via upload

---
+++ PhtythonProfile.py
@@ -0,0 +1,20 @@
+name = "Bobby"
+age = 11
+weight = 39.8
+is_student = True
+
+print("Name :", name)
+print("DataType of Name is  :" , type(name))
+print("Age :", age)
+print("DataType of Name is  :" , type(age))
+print("Weight :", weight)
+print("DataType of Name is  :" , type(weight))
+print("Is he a Student? :", is_student)
+print("DataType of Name is  :" , type(is_student))
+
+print("/n After Type Casting.....")
+age = str(age)
+print("DataType of A is : " , type(age))
+weight = int(weight)
+print(weight)
+print("DataTpe of Weight is ", type(weight))
