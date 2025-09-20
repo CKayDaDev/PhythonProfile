5c553a221c3355a0f6469fa4f862c37d7d77a484
CKayDaDev <chikeechendu@gmail.com>
2025-09-20 17:28:31 +0100

    Add files via upload

---
+++ Profit.py
@@ -0,0 +1,6 @@
+actual_cost = float(input("Pls type the Actual cost of this it"))
+sales_cost = float(input("PLs put your actual sales cost of the item"))
+if actual_cost > sales_cost:
+    print("Profit Lost")
+else:
+    print("Profit earnt")
