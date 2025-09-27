7477858d78291e5761f27b5b84fe09ed8ada699a
CKayDaDev <chikeechendu@gmail.com>
2025-09-27 17:46:16 +0100

    Add files via upload

---
+++ AndOrOperator.py
@@ -0,0 +1,11 @@
+a = 10
+b = 24
+c = 10
+if c == 5 or b == 24:
+    print("This statement is true")
+else:
+    print("This statement is false")
+if a == 1 and b == 24:
+    print("This statement is true")
+else:
+    print("This statement is false")
